2014f4778116f241dcc9444ea2b344fac0014b33
Rasmus <rasm96e8@edu.ucl.dk>
2019-09-25 13:58:15 +0200

    Sten...udenparse

--- StenSaksPapir/StenSaksPapir/Program.cs
+++ StenSaksPapir/StenSaksPapir/Program.cs
@@ -10,7 +10,87 @@ namespace StenSaksPapir
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test:D");
+
+            Console.WriteLine("indtast spiller 1 ");
+            string spiller1 = Console.ReadLine();
+            Console.WriteLine("indtast spiller 2 ");
+            string spiller2 = Console.ReadLine();
+
+
+
+          
+            Console.WriteLine(" vælg dit våben " + spiller1);
+            Console.WriteLine(" 1:Sten \n 2:Saks \n 3:Papir");
+
+
+            int våben1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine(" vælg dit våben " + spiller2);
+            Console.WriteLine(" 1:Sten \n 2:Saks \n 3:Papir");
+            int våben2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+          
+
+            //Uafgjort
+            if (våben1 == våben2)
+            {
+                Console.WriteLine(" Kampen er uafgjort prøv igen. ");
+            }
+
+            //sten vs saks
+            else if (våben1 == 1 && våben2 == 2)
+            {
+                Console.WriteLine(spiller1 + " Har vundet! ");
+            }
+            else if (våben1 == 2 && våben2 == 1)
+            {
+                Console.WriteLine(spiller2 + " Har vundet! ");
+            }
+
+            //sten vs papir
+            else if (våben1 == 1 && våben2 == 3)
+            {
+                Console.WriteLine(spiller2 + " Har vundet! ");
+            }
+            else if (våben1 == 3 && våben2 == 1)
+            {
+                Console.WriteLine(spiller1 + " Har vundet! ");
+            }
+
+            //papir vs saks
+            else if (våben1 == 2 && våben2 == 3)
+            {
+                Console.WriteLine(spiller1 + " Har vundet! ");
+            }
+            else if (våben1 == 3 && våben2 == 2)
+            {
+                Console.WriteLine(spiller2 + " Har vundet! ");
+            }
+
+            else if (våben1 > 3 || våben2 > 3)
+            {
+                Console.WriteLine(" der var en forkert værdi spillet kan ikke forsætte ");
+            }
+
+
+
+
+
+            Console.Read();
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
     }
