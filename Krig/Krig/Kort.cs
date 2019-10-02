b19687457974c6a835f3e30613850a8cb6853050
Rasmus Schou Heidelbach <rasm96e8@edu.ucl.dk>
2019-10-02 12:05:23 +0200

    Kort og spiller klasser

---
+++ Krig/Krig/Kort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krig
+{
+
+    public enum Kulør
+    {
+        HJERTER,
+        KLØR,
+        SPAR,
+        RUDER,
+    }
+
+    public enum Værdi
+    {
+        two = 2, tre = 3, fire = 4, fem = 5, seks = 6, syv = 7, otte = 8,
+        ni = 9, ti = 10, knægt = 11, droning = 12, konge = 13, es = 14
+    }
+
+    public class Kort
+    {       
+       
+        public Kulør Kulør
+        {
+            get;
+            set;
+
+        }
+        public Værdi Værdi
+        {
+            get;
+            set;
+        }
+
+
+
+
+    }
+}
+
